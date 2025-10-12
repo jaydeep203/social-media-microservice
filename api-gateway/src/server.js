fe2bc10ae4663437700eea60ca1dd6aca4f3f2f9
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2025-10-12 09:15:01 +0530

    Github actions added for CI/CD.

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -125,5 +125,5 @@ app.listen(PORT, ()=>{
     logger.info(`Media service is running on port ${process.env.MEDIA_SERVICE_URL}`);
     logger.info(`Search service is running on port ${process.env.SEARCH_SERVICE_URL}`);
     logger.info(`Redis URL ${process.env.REDIS_URL}`);
-})
+});
  
